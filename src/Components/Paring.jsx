f2fd3a689d2322e6b8da89c5564d79b4128cbae4
ahad18 <abdulahad6361@gmail.com>
2024-04-23 13:56:00 +0500

    new

--- src/Components/Paring.jsx
+++ src/Components/Paring.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-let text = '';
-const ahadAbdul = ['ac', 'cd', 'ef', 'gh', 'ij'];
-
-const cid = (val, index) => {
-  text += index + ':' + val + '; ';
-};
-
-ahadAbdul.forEach(cid);
+const AhadAbdul = ['ac', 'cd', 'ef', 'gh', 'ij'];
 
 function Paring() {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    let newText = '';
+    AhadAbdul.forEach((val, index) => {
+      newText += index + ':' + val + '; ';
+    });
+    setText(newText);
+  }, []);
+
   return (
-    <div>
-      <h1>{text}</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">{text}</h1>
+      <h1 className="text-xl font-semibold mb-4">Ride it</h1>
+      <audio className="mb-4" controls>
+        <source src="https://aac.saavncdn.com/805/4d903cc84643cb2971d9d57b1f146ae8_320.mp4" type="audio/mpeg" />
+        
+      </audio>
     </div>
   );
 }
