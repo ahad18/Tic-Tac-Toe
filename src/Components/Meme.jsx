885493b88216c3fa927ef7d3c7b67621ceb9b442
Abdul Ahad sharif <111515779+ahad18@users.noreply.github.com>
2024-10-14 03:53:47 +0500

    nn

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import Swal from 'sweetalert2'
+import React from 'react';
+import Swal from 'sweetalert2';
+import soundMeme from '../assets/soundmeme.mp3'; // Properly import the sound file
 
 function Meme() {
-    const sound = new Audio('src/assets/soundmeme.mp3')
-    
+    const sound = new Audio(soundMeme); // Use the imported sound file
+
     const checkAge = () => {
-        let age = parseInt(document.getElementById('checkage').value)
+        let age = parseInt(document.getElementById('checkage').value);
         if (age >= 18){
-            sound.play()
+            sound.play(); // Play sound on valid condition
         } else {
             Swal.fire({
                 title: 'Access Denied!',
                 text: 'You are too young',
                 icon: 'error'
-            })
+            });
         }
     }
 
     return (
         <>
         <div className='container flex justify-center'> 
-            <input className='p-2 border-2 rounded' type="number" name="" id="checkage" />
+            <input className='p-2 border-2 rounded' type="number" id="checkage" />
             <button className='p-2 border-2 rounded' onClick={checkAge}>Check Age</button>
-            
-            </div>
+        </div>
         </>
-    )
+    );
 }
 
-export default Meme
+export default Meme;
